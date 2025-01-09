bf43d81d7ee5d13e9bb48f12b28a9da09d62d41b
Julia123456789037 <julia037.filia@gmail.com>
2025-01-09 09:49:28 +0100

    tout les bouton de la maquette, pas lier avec controleur

--- src/application/vue/BarreNav.java
+++ src/application/vue/BarreNav.java
@@ -22,8 +22,14 @@ public class BarreNav extends JToolBar implements ActionListener{
 
 	private Controleur		ctrl;
 	private JPanel			panel;
+	private JButton			btnLogo;
 	private JButton			btnPageArriere;
 	private JButton			btnPageAvant;
+
+	private JButton			btnTexte1;
+	private JButton			btnTexte2;
+	private JButton			btnParametreAnalyse;
+	private JButton			btnAnalyse;
 
 	public BarreNav(Controleur ctrl) {
 
@@ -36,35 +42,60 @@ public class BarreNav extends JToolBar implements ActionListener{
 		/* Création des composants       */
 		/*-------------------------------*/
 
+		this.btnLogo = new JButton(new ImageIcon("/Logo.png"));
+		this.btnLogo.setToolTipText("Ecureuil");
+		this.btnLogo.setActionCommand("logo");
 
 		this.btnPageArriere = new JButton(new ImageIcon("/undo.png"));
-		this.btnPageArriere.setToolTipText("Défaire");
+		this.btnPageArriere.setToolTipText("Page arrière");
 		this.btnPageArriere.setActionCommand("pageArriere");
 		
 		this.btnPageAvant = new JButton(new ImageIcon("/redo.png"));
-		this.btnPageAvant.setToolTipText("Refaire");
+		this.btnPageAvant.setToolTipText("Page Avant");
 		this.btnPageAvant.setActionCommand("pageAvant");
 
+		this.btnTexte1 = new JButton(new ImageIcon("/Logo.png"));
+		this.btnTexte1.setToolTipText("Nouveau texte");
+		this.btnTexte1.setActionCommand("compare");
+
+		this.btnTexte2 = new JButton(new ImageIcon("/undo.png"));
+		this.btnTexte2.setToolTipText("Texte comparant");
+		this.btnTexte2.setActionCommand("comaprants");
+		
+		this.btnParametreAnalyse = new JButton(new ImageIcon("/redo.png"));
+		this.btnParametreAnalyse.setToolTipText("Paramêtre de l'analyse");
+		this.btnParametreAnalyse.setActionCommand("parametreAna");
+
+		this.btnAnalyse = new JButton(new ImageIcon("/redo.png"));
+		this.btnAnalyse.setToolTipText("Analyse");
+		this.btnAnalyse.setActionCommand("analyse");
 
 		/*-------------------------------*/
 		/* Positionnement des composants */
 		/*-------------------------------*/
 		
 		this.panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		
+
+		this.panel.add( this.btnLogo );
 		this.panel.add( this.btnPageArriere );
 		this.panel.add( this.btnPageAvant );
-
+		this.panel.add( this.btnTexte1 );
+		this.panel.add( this.btnTexte2 );
+		this.panel.add( this.btnParametreAnalyse );
+		this.panel.add( this.btnAnalyse );
 
 		this.add(panel, BorderLayout.CENTER);
-
 
 		/*-------------------------------*/
 		/* Activation des composants     */
 		/*-------------------------------*/
-		this.btnPageArriere	.addActionListener(this);
-		this.btnPageAvant	.addActionListener(this);
-		
+		this.btnLogo				.addActionListener(this);
+		this.btnPageArriere			.addActionListener(this);
+		this.btnPageAvant			.addActionListener(this);
+		this.btnTexte1				.addActionListener(this);
+		this.btnTexte2				.addActionListener(this);
+		this.btnParametreAnalyse	.addActionListener(this);
+		this.btnAnalyse				.addActionListener(this);
 	}
 
 
@@ -73,8 +104,13 @@ public class BarreNav extends JToolBar implements ActionListener{
 	{
 			
 		switch (e.getActionCommand()) {
+			case "Logo"	-> { }
 			case "pageArriere"	-> { }
 			case "pageAvant"	-> { }
+			case "compare"	-> { }
+			case "comaprants"	-> { }
+			case "parametreAna"	-> { }
+			case "analyse"	-> { }
 		}
 		this.updateUI();
 	}
