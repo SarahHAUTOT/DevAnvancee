f1b2adf6cd4bf0fa839c68864687a5759f8ef029
Ottirate <sarah.hautot76@gmail.com>
2025-01-16 09:17:39 +0100

    Q

--- src/application/vue/FrameAccueil.java
+++ src/application/vue/FrameAccueil.java
@@ -99,16 +99,17 @@ public class FrameAccueil extends JFrame implements ActionListener
 		panelPrincipale.setLayout(new BorderLayout());
 
 		// Panel Titre
-		panelPrincipale.add(this.panelTitre("Texte suspecté de plagiat"), BorderLayout.NORTH);
+		// panelPrincipale.add(this.panelTitre("Texte suspecté de plagiat"), BorderLayout.NORTH);
 
         // Panel Central
-        panelPrincipale.add(this.panelSaisieTexte(0.65, 0.6), BorderLayout.CENTER);
+        // panelPrincipale.add(this.panelSaisieTexte(0.65, 0.6), BorderLayout.CENTER);
+        panelPrincipale.add(new PanelResultat(this), BorderLayout.CENTER);
 
         // Bouton "Suivant"
 		JPanel panelSuivant = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		panelSuivant.add(this.btnSuivant);
 		panelSuivant.setBackground(FrameAccueil.COULEUR_FOND);
-        panelPrincipale.add(panelSuivant, BorderLayout.SOUTH);
+        // panelPrincipale.add(panelSuivant, BorderLayout.SOUTH);
 
 		// Panel Bordure gauche 
 		JPanel panelBordure = new JPanel();
@@ -133,11 +134,30 @@ public class FrameAccueil extends JFrame implements ActionListener
 	 * @param titre affiché sur le panel
 	 * @return un panel
 	 */
-	private JPanel panelTitre(String titre)
+	JPanel panelTitre(String titre)
 	{
 		JPanel panelTitre = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel titreLbl = new JLabel(titre);
 		titreLbl.setFont(new Font(FrameAccueil.POLICE_TEXTE, Font.BOLD, 24));
+
+		titreLbl  .setForeground(FrameAccueil.COULEUR_SECONDAIRE);	// Couleur du texte
+		panelTitre.setBackground(FrameAccueil.COULEUR_FOND);		// Couleur du fond
+
+		panelTitre.add(titreLbl);
+		
+		return panelTitre;
+	}
+
+	/**
+	 * Création du panel qui décrit l'étape
+	 * @param titre affiché sur le panel
+	 * @return un panel
+	 */
+	JPanel panelTexte(String titre)
+	{
+		JPanel panelTitre = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JLabel titreLbl = new JLabel(titre);
+		titreLbl.setFont(new Font(FrameAccueil.POLICE_TEXTE, Font.BOLD, 12));
 
 		titreLbl  .setForeground(FrameAccueil.COULEUR_SECONDAIRE);	// Couleur du texte
 		panelTitre.setBackground(FrameAccueil.COULEUR_FOND);		// Couleur du fond
@@ -264,6 +284,11 @@ public class FrameAccueil extends JFrame implements ActionListener
 	public List<String> getLstPlagiatDetecte()
 	{
 		return new ArrayList<>(); // TODO : RELIER AU CONTROLEUR POUR RECUPERER LES PHRASE QUI ONT ETAIT DETECTE COMME PLAGIE
+	}
+
+	public String getComparant()
+	{
+		return "Once upon a time there was a lovely princess. But she had an enchantment upon her of a fearful sort, which could only be broken by Love's first kiss. She was locked away in a castle guarded by a terrible fire breathing dragon. Many brave knights had attempted to free her from this dreadful prison, but none prevailed. She waited in the dragon's keep in the highest room of the tallest tower for her true love and true love's first kiss. Like that's ever going to happen. What a loony. Shrek Beware Stay out I think he's in here. All right. Lets get it! Hold on. Do you know what that thing can do to you? Yeah. He'll groan into your bones for his brains. Well actually that would be a giant. Now Ogres, huh, they are much worse. They'll make a soup from your freshly peeled skin. They'll chew your livers, squeeze the jelly from your eyes. Actually, it's quite good on toast. Back, back beast, back! I warned you! Right. This is the part, where you run away. Yeah! And stay out. Wanted. Fairytale creatures. Right, this one is full. Take it away. Give me that. Your fine days are over. -25 pieces of silver for the witch. Next. -Come on. Sit down there! And be quiet! This cage is so small.";
 	}
 	
 	public static void main(String[] args)
